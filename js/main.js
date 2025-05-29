152699f85b852a3a94bf1955ab80723414149918
unknown <artur01049718@gmail.com>
2025-05-29 21:36:25 +0200

    try to create

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Main JavaScript file
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Crazy Parking Jam website loaded!");
+
+  // Import page-specific JavaScript
+  const currentPage = window.location.pathname;
+
+  if (
+    currentPage.endsWith("command-center.html") ||
+    currentPage.endsWith("/")
+  ) {
+    import("./command-center.js");
+  } else if (currentPage.includes("warzone-updates.html")) {
+    import("./warzone-updates.js");
+  } else if (currentPage.includes("mission-briefing.html")) {
+    import("./mission-briefing.js");
+  } else if (currentPage.includes("secure-comms.html")) {
+    import("./secure-comms.js");
+  } else if (currentPage.includes("intel-protocols.html")) {
+    import("./intel-protocols.js");
+  } else if (currentPage.includes("arsenal-loadout.html")) {
+    import("./arsenal-loadout.js");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bar = document.getElementById("cookie-bar");
+  const btn = document.getElementById("cookie-accept");
+
+  if (!localStorage.getItem("cookieAccepted")) {
+    bar.style.display = "flex";
+  }
+
+  btn.addEventListener("click", () => {
+    localStorage.setItem("cookieAccepted", "true");
+    bar.style.display = "none";
+  });
+});
